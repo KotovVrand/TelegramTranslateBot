9c2bbc392131d8f3f8972afd9965e60fbc6ac691
KotovBrand <vladislavkotovfx@gmail.com>
2025-06-05 17:58:40 +0300

    Update translate_bot.py

--- translate_bot.py
+++ translate_bot.py
@@ -1,12 +1,11 @@
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from telegram import Update
-from deep_translator import GoogleTranslator
+from deep_translator import LibreTranslator
 import logging
 
-# Заміни на свій токен або передай як змінну середовища
+# Заміни токен на свій
 TOKEN = '7911165186:AAEHFfxvlitKeGMXQSxC1qQphqejN7lLFZA'
 
-# Логи для Railway
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -16,11 +15,11 @@ async def translate_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_text = update.message.text
     try:
         translations = {
-            'English': GoogleTranslator(source='auto', target='en').translate(message_text),
-            'Russian': GoogleTranslator(source='auto', target='ru').translate(message_text),
-            'French': GoogleTranslator(source='auto', target='fr').translate(message_text),
-            'Italian': GoogleTranslator(source='auto', target='it').translate(message_text),
-            'Japanese': GoogleTranslator(source='auto', target='ja').translate(message_text)
+            'English': LibreTranslator(source='auto', target='en').translate(message_text),
+            'Russian': LibreTranslator(source='auto', target='ru').translate(message_text),
+            'French': LibreTranslator(source='auto', target='fr').translate(message_text),
+            'Italian': LibreTranslator(source='auto', target='it').translate(message_text),
+            'Japanese': LibreTranslator(source='auto', target='ja').translate(message_text)
         }
 
         reply_text = "🫡\n\n"
@@ -36,7 +35,6 @@ async def translate_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate_message))
-
     logging.info("Bot is running...")
     app.run_polling()
 
